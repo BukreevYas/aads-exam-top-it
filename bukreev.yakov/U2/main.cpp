0ac894303b11a7eb09fba7349332bcf6d0409673
BukreevYas <bukreev.yas@edu.spbstu.ru>
2026-06-25 14:10:12 -0400

    Wrap tree logic in namespace and create id tree

--- bukreev.yakov/U2/main.cpp
+++ bukreev.yakov/U2/main.cpp
@@ -1,2 +1,11 @@
+#include "tree.hpp"
+#include <cstdlib>
+
 int main()
-{}
+{
+  using namespace bukreev;
+
+  Tree< size_t >* idTree;
+  createTree(&idTree);
+  deleteTree(idTree);
+}
--- bukreev.yakov/U2/tree.hpp
+++ bukreev.yakov/U2/tree.hpp
@@ -1,69 +1,72 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
-template< class T >
-struct Tree
+namespace bukreev
 {
-  T val;
-  Tree< T >* left;
-  Tree< T >* right;
-  Tree< T >* parent;
-};
-
-template< class T >
-void createTree(Tree< T >** ppTree)
-{
-  *ppTree = new Tree< T >;
-  Tree< T >* pTree = *ppTree;
-  pTree->parent = nullptr;
-  pTree->left = nullptr;
-  pTree->right = nullptr;
-}
-
-template< class T >
-void deleteTree(Tree< T >* tree)
-{
-  if (tree->left)
+  template< class T >
+  struct Tree
   {
-    deleteTree(tree->left);
-  }
-  if (tree->right)
+    T val;
+    Tree< T >* left;
+    Tree< T >* right;
+    Tree< T >* parent;
+  };
+
+  template< class T >
+  void createTree(Tree< T >** ppTree)
   {
-    deleteTree(tree->right);
+    *ppTree = new Tree< T >;
+    Tree< T >* pTree = *ppTree;
+    pTree->parent = nullptr;
+    pTree->left = nullptr;
+    pTree->right = nullptr;
   }
 
-  delete tree;
-}
-
-template< class T >
-void insertTree(Tree< T >* tree, T& val)
-{
-  Tree< T >* parent;
-  while (tree)
+  template< class T >
+  void deleteTree(Tree< T >* tree)
   {
-    parent = tree;
-    if (val > tree->val)
+    if (tree->left)
     {
-      tree = tree->right;
+      deleteTree(tree->left);
+    }
+    if (tree->right)
+    {
+      deleteTree(tree->right);
+    }
+
+    delete tree;
+  }
+
+  template< class T >
+  void insertTree(Tree< T >* tree, T& val)
+  {
+    Tree< T >* parent;
+    while (tree)
+    {
+      parent = tree;
+      if (val > tree->val)
+      {
+        tree = tree->right;
+      }
+      else
+      {
+        tree = tree->left;
+      }
+    }
+
+    Tree< T >* newNode = new Tree< T >;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    newNode->parent = parent;
+
+    if (val > parent->val)
+    {
+      parent->right = newNode;
     }
     else
     {
-      tree = tree->left;
+      parent->left = newNode;
     }
-  }
-
-  Tree< T >* newNode = new Tree< T >;
-  newNode->left = nullptr;
-  newNode->right = nullptr;
-  newNode->parent = parent;
-
-  if (val > parent->val)
-  {
-    parent->right = newNode;
-  }
-  else
-  {
-    parent->left = newNode;
   }
 }
 
